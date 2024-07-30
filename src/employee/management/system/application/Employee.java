f726a0d3a9032fa6a1dbf1a2306c43a5786e8afa
shahbazkhan520441 <shahbazkhan520441@gmail.com>
2024-07-30 12:32:03 +0530

    created feature for employee attendance

--- src/employee/management/system/application/Employee.java
+++ src/employee/management/system/application/Employee.java
@@ -3,5 +3,15 @@ package employee.management.system.application;
 public class Employee {
 	public static void main(String[] args) {
 	System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+	
+	int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+    if(empCheck == 0)
+    {
+        System.out.println("Employee is Present");
+    }
+    else
+    {
+        System.out.println("Employee is Absent");
+    }
 }
 }
