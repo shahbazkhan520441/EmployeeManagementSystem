d31cea816331ad042a86a00e124fb19e5f1e9e89
shahbazkhan520441 <shahbazkhan520441@gmail.com>
2024-07-30 12:17:27 +0530

    firstcommit

---
+++ src/employee/management/system/application/Employee.java
@@ -0,0 +1,7 @@
+package employee.management.system.application;
+
+public class Employee {
+	public static void main(String[] args) {
+	System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+}
+}
